ce645a9187733535f3248150632934073b496935
justinjensen <justinjensen@users.noreply.github.com>
2024-11-25 00:14:08 -0700

    exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,12 +1,13 @@
-const dummy = (blogs) => {
-  return 1
-}
-
 const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return undefined
+  return blogs.reduce((max, blog) => (max.likes > blog.likes ? max : blog))
+}
+
 export default {
-  dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
